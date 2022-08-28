9dd7234511929bbaee5aa42bdca7a80ac13e3ede
Yasmin Carlôto <yasmincarlotobezerra17@gmail.com>
2022-08-28 14:17:37 -0300

    JavaScript com apagar, apagarTudo e vazio

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function inserir(num){
-    document.getElementById('resultado').innerHTML += num;
+function inserir (num) {
+    const resultado = document.getElementById('resultado');
+    if(resultado.innerHTML == '0'){
+        resultado.innerHTML += num;
+        resultado.innerHTML = resultado.innerHTML.substring(1)
+    }
+    else{
+        resultado.innerHTML += num;
+    }
 }
 
 function numeros(i){
@@ -8,5 +15,22 @@ function numeros(i){
         if (input[i].classList != 'operador'){
             input[i].addEventListener('click', function(){inserir(input[i].value);})
         }
+    }
+}
+
+function apagar (){
+    const resultado = document.getElementById('resultado');
+    const tamanho = resultado.innerHTML.length;
+    resultado.innerHTML = resultado.innerHTML.substring(0,tamanho-1)
+}
+
+function apagarTudo () {
+    document.getElementById('resultado').innerHTML = "";
+}
+
+async function vazio(){
+    const resultado = document.getElementById('resultado');
+    if (Boolean(resultado.innerHTML) == false){
+        resultado.innerHTML = "0"
     }
 }
