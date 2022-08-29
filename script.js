ddf5bc4d26d0d47662f47288707758e1b8df9269
Daniel02md <gustanunespp157@gmail.com>
2022-08-29 00:14:48 -0300

    Alterando e atualizando o funcionamento da função inserir

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
 function inserir(i){
     const resultado = document.getElementById('resultado');
     if (resultado.innerHTML == "0"){
-        resultado.innerHTML += i;
-        resultado.innerHTML = resultado.innerHTML.substring(1);
-    }else{       
-        resultado.innerHTML += i;
+        if (i=="."){
+            resultado.innerHTML += i;
+        }else{
+            resultado.innerHTML += i;
+            resultado.innerHTML = resultado.innerHTML.substring(1);
+        }
+    }else{
+        if (i == "."){
+            const listaTexto = Array.from(resultado.innerHTML);
+            if(! listaTexto.find(char => char=='.')){
+                resultado.innerHTML += i;
+            }
+        }else{
+            resultado.innerHTML += i;
+        }
+
     }
- }
- 
- function numeros(){
-     const input = Array.from(document.getElementsByTagName('input'));
- 
-     input.map(
-         function (element){
-             if (element.classList[0] != 'operador'){
-             element.onclick = function (){inserir(element.value);}}
-         })
- }
- 
- 
- function apagarTudo(){
-     document.getElementById('resultado').innerHTML = "" 
- }
- 
- function apagar(){
+}
+
+function numeros(){
+    const input = Array.from(document.getElementsByTagName('input'));
+
+    input.map(
+        function (element){
+            if (element.classList[0] != 'operador'){
+                element.onclick = function (){inserir(element.value);}}
+        })
+}
+
+
+function apagarTudo(){
+    document.getElementById('resultado').innerHTML = "" 
+}
+
+function apagar(){
     const resultado = document.getElementById('resultado');
     const tamanho = resultado.innerHTML.length;
     resultado.innerHTML = resultado.innerHTML.substring(0, tamanho-1)
- }
- 
- async function checarVazio(){
-     
-     const resultado = document.getElementById('resultado');
-     if (Boolean(resultado.innerHTML) == false){
-         resultado.innerHTML = "0"
-     }
- }
+}
+
+function checarVazio(){
+    const resultado = document.getElementById('resultado');
+    
+    if (Boolean(resultado.innerHTML) == false){
+        resultado.innerHTML = "0"
+    }
+}
